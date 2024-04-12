fc6292fb30962fdbea6e5ed0a763e023feb293a5
Jonathan Perry <jpper@umich.edu>
2024-04-11 23:58:58 -0400

    Update link to resume in dialog

--- src/constants.js
+++ src/constants.js
@@ -11,7 +11,7 @@ export const dialogueData = {
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
      <a href="https://www.youtube.com/@CinnamonToastKen" target="_blank">CinnamonToastKen</a>, and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
   bed: `This where I sleep. Great ideas sometimes come when I'm lying on my bed! When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/jonathanpperry/resume/blob/main/Jonathan_Perry_Resume.pdf" target="_blank">Check it out!</a>
+  resume: `This is my desk and on it is my resume. <a href="https://jonathanpperry.github.io/files/Resume.pdf" target="_blank">Check it out!</a>
     Contact me at jpper@umich.edu if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
